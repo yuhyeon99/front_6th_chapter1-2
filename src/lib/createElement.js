2acab2d800914d00c383f1f2d930cea7eae1894a
yuhyeon99 <jujini31@gmail.com>
2025-07-15 22:40:46 +0900

    feat: createElement에서 null/undefined/boolean 입력을 빈 텍스트 노드로 처리

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,10 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  // 1. null, undefined, boolean -> 빈 텍스트 노드
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
+}
 
-function updateAttributes($el, props) {}
+// function updateAttributes($el, props) {}
