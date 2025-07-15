ca8faaae5beaf5467082c4a874b685628efe7733
kyheroumtech <jujini31@gmail.com>
2025-07-15 16:14:34 +0900

    feat: 가상 DOM 정규화를 위한 normalizeVNode 함수 구현
    - null, undefined, boolean, string, number 타입의 vnode 처리
    - 함수형 컴포넌트는 호출 후 결과를 재귀적으로 정규화
    - 일반 객체 vnode의 경우 props와 children을 재귀적으로 정규화
    - 자식 노드는 isValidChild로 필터링하여 유효하지 않은 값 제거

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,24 @@
+import { isValidChild } from "../utils/common/validChildren";
+
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (typeof vNode === "object") {
+    if (typeof vNode.type === "function") {
+      // vNode가 함수형 컴포넌트일 경우, 해당 컴포넌트를 호출해서 그 결과를 다시 정규화.
+      return normalizeVNode(vNode.type({ ...vNode.props, children: vNode.children }));
+    }
+
+    return {
+      type: vNode.type,
+      props: vNode.props,
+      children: vNode.children.map(normalizeVNode).filter(isValidChild),
+    };
+  }
 }
