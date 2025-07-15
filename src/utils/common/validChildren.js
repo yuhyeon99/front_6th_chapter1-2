e4f5f3a6377ec18c01022fd1c4483e13c4a36f32
kyheroumtech <jujini31@gmail.com>
2025-07-15 16:12:22 +0900

    fix: isValidChild에 빈 문자열('') 필터링 조건 추가
    - ''(빈 문자열)을 유효하지 않은 child로 간주하도록 isValidChild 조건 수정
    - normalizeVNode 및 createVNode에서 의미 없는 빈 텍스트 노드 제거 목적

--- src/utils/common/validChildren.js
+++ src/utils/common/validChildren.js
@@ -1,3 +1,3 @@
 export function isValidChild(child) {
-  return child !== undefined && child !== null && child !== false;
+  return child !== undefined && child !== null && child !== false && child !== "";
 }
