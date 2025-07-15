d117b2c53c25e971e59ebdb741f328670d5fa67d
yuhyeon99 <jujini31@gmail.com>
2025-07-15 22:44:19 +0900

    feat: createElement 문자열과 숫자 입력에 대해 텍스트 노드로 변환하는 로직 추가

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -5,6 +5,14 @@ export function createElement(vNode) {
   if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
     return document.createTextNode("");
   }
+
+  // 2. string, number -> 텍스트 노드
+  //    - 숫자, 문자열은 텍스트 노드로 변환
+  //    - 숫자는 문자열로 변환하여 처리
+  //    - 문자열은 그대로 사용
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(String(vNode));
+  }
 }
 
 // function updateAttributes($el, props) {}
